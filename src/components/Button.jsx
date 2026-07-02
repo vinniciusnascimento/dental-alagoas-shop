cbc794145532522a594ab858358c555c9a9df972
Vinnicius Nascimento <calangowproducer@gmail.com>
2026-07-01 23:21:19 -0300

    fix: style MainSection

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,8 @@
+import { ArrowUpRight } from "lucide-react"
+
 function Button(props) {
     return(
-        <button className={`font-inter rounded-3xl font-bold text-white bg-blue-primary px-14 py-5 cursor-pointer transition-all hover:bg-transparent border-4 border-blue-primary hover:scale-102 ${props.className}`}>{props.content}</button>
+        <button className={`flex flex-row items-center gap-2 font-inter rounded-3xl font-bold text-white bg-blue-primary px-11 py-5 cursor-pointer transition-all hover:bg-transparent border-4 border-blue-primary hover:scale-102 ${props.className}`}>{props.content} <ArrowUpRight /></button>
     )
 }
 
